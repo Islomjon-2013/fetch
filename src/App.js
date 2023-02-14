7367c3f0227033b628a7735d650314b42932eb65
hulkar <hulkar@gmail.com>
2023-02-14 16:56:40 -0500

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { useEffect,useState } from 'react';
 import './App.css';
 
 function App() {
+  const[fake,setFake]=useState([])
+  useEffect(()=>{
+    faceStore()
+  },[])
+
+  const faceStore = async()=>{
+    const response=await fetch(" https://fakestoreapi.com/products")
+    console.log(response)
+    const jsonData=await response.json()
+    setFake(jsonData)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<h2>Fake API store</h2>
+<div className="container">
+  {fake.map((v)=>{
+    return(
+      <>
+<div className="box">
+<div className="content">
+  <h5>{v.title}</h5>
+  <p>{v.description}</p>
+</div>
+<img src={v.image} alt="" />
+</div>
+      
+      </>
+    )
+  })}
+
+
+</div>
+    </>
   );
 }
 
